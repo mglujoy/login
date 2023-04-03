b39175a022b383af355b872556f13479e32cbe9d
mglujoy <mglujoy@gmail.com>
2023-04-03 18:38:53 -0300

    working CORS --MG

--- src/main/java/com/portfolio/login/controller/UserController.java
+++ src/main/java/com/portfolio/login/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.portfolio.login.controller;
 import com.portfolio.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class UserController {
     @Autowired
     private UserService userservice;
     
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("user/{username}/{password}")
     public int UserLogin(@PathVariable("username") String username1, 
                          @PathVariable("password") String password1) {
